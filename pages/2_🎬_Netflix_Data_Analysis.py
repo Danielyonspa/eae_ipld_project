156e33a516652dc02835da6ffa2ccb5a63d7ef87
DanielyonsPa <lyons42@gmail.com>
2024-01-04 19:18:13 +0100

    2.3

--- pages/2_🎬_Netflix_Data_Analysis.py
+++ pages/2_🎬_Netflix_Data_Analysis.py
@@ -51,7 +51,7 @@ max_year = movies_df ['release_year'].max( )
 
 
 # TODO: Ex 2.3: How many director names are missing values (NaN)?
-num_missing_directors = movies_df.groupby("Missing Directos Names")["director"].count()
+num_missing_directors = None
 
 # TODO: Ex 2.4: How many different countries are there in the data?
 n_countries = None
